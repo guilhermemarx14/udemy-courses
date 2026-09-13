1fe7da895e0c6244a9f498e549c3e14c169d62a3
Guilherme Marx Ferreira Tavares <guilhermemarx14@gmail.com>
2026-09-13 05:18:09 -0300

    Produto c$

---
+++ c#/Produto/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Produto {
+     class Produto {
+        public string Nome;
+        public int Quantidade;
+        public double Preco;
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString() {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
